2cd0fffce845e6802e385c6264617d178beb03b7
tiarasopiyanti <118708571+tiarasopiyanti@users.noreply.github.com>
2022-11-29 15:35:40 +0700

    Add files via upload

---
+++ Tiara Sopiyanti/tugasdpl.js
@@ -0,0 +1,6 @@
+//mengubah jam dan menit kedalam satuan detik
+var panggilUntukInput = require('readline-sync');
+var jam = panggilUntukInput.questionInt("Input jam: ");
+console.log(jam*3600 + "Detik");
+var menit = panggilUntukInput.questionInt("Input Menit: ");
+console.log(menit*60 + "Detik");
